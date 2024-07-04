6fc643c0ea351224829fd171ca4d3867b0de8586
pc <poyatajhiz59@gmail.com>
2024-07-04 15:46:14 -0800

    sorted

--- src/main/java/com/example/finalprojectthirdphase/entity/SubDuty.java
+++ src/main/java/com/example/finalprojectthirdphase/entity/SubDuty.java
@@ -20,7 +20,7 @@ import java.util.List;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
-@JsonIdentityInfo(scope = SubDuty.class,generator= ObjectIdGenerators.PropertyGenerator.class, property="id")
+@JsonIdentityInfo(scope = SubDuty.class, generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class SubDuty {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
