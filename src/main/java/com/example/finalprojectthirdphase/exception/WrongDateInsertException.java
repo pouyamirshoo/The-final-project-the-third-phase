efc72c8ed69a55a0555572a6a9aad91b4a6f7bd1
pc <poyatajhiz59@gmail.com>
2024-07-04 16:31:49 -0800

    create and fill WrongDateInsertException extended class

---
+++ src/main/java/com/example/finalprojectthirdphase/exception/WrongDateInsertException.java
@@ -0,0 +1,7 @@
+package com.example.finalprojectthirdphase.exception;
+
+public class WrongDateInsertException extends RuntimeException {
+    public WrongDateInsertException(String message) {
+        super(message);
+    }
+}
