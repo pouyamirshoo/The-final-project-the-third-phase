2a808e721a413c0a3f3cef28a4ca2cda6e44b1b1
pc <poyatajhiz59@gmail.com>
2024-07-04 16:25:29 -0800

    create and fill RequestMapper interface for make Connect between Request Entity and DTOs

---
+++ src/main/java/com/example/finalprojectthirdphase/mapper/RequestMapper.java
@@ -0,0 +1,17 @@
+package com.example.finalprojectthirdphase.mapper;
+
+import com.example.finalprojectthirdphase.dto.RequestReturn;
+import com.example.finalprojectthirdphase.dto.RequestSaveRequest;
+import com.example.finalprojectthirdphase.entity.Request;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(uses = SubDutyMapper.class)
+public interface RequestMapper {
+
+    RequestMapper INSTANCE = Mappers.getMapper(RequestMapper.class);
+
+    Request requestSaveRequestToModel(RequestSaveRequest request);
+
+    RequestReturn modelRequestToSaveResponse(Request request);
+}
