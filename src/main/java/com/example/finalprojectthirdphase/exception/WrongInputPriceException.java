c437485041a1c15a5e6394618e6229adde123a33
pc <poyatajhiz59@gmail.com>
2024-07-04 16:32:31 -0800

    create and fill WrongInputPriceException extended class

---
+++ src/main/java/com/example/finalprojectthirdphase/exception/WrongInputPriceException.java
@@ -0,0 +1,7 @@
+package com.example.finalprojectthirdphase.exception;
+
+public class WrongInputPriceException extends RuntimeException {
+    public WrongInputPriceException(String message) {
+        super(message);
+    }
+}
