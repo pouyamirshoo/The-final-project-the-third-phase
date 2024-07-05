0dcfc12afdf28af1f53ba8b48a1b484380156614
pc <poyatajhiz59@gmail.com>
2024-07-04 17:49:47 -0800

    create FinalProjectThirdPhaseApplication main Class

---
+++ src/main/java/com/example/finalprojectthirdphase/FinalProjectThirdPhaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalprojectthirdphase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectThirdPhaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectThirdPhaseApplication.class, args);
+    }
+
+}
