5130dd933a3768d3f20fc83d49c97e1874f629bf
pc <poyatajhiz59@gmail.com>
2024-07-04 16:28:54 -0800

    create and fill DuplicateInformationException extended class

---
+++ src/main/java/com/example/finalprojectthirdphase/exception/DuplicateInformationException.java
@@ -0,0 +1,7 @@
+package com.example.finalprojectthirdphase.exception;
+
+public class DuplicateInformationException extends RuntimeException {
+    public DuplicateInformationException(String message) {
+        super(message);
+    }
+}
