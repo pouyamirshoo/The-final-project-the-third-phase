b2b6224a761a419127d39568e91f86d1edf3978c
pc <poyatajhiz59@gmail.com>
2024-07-04 16:51:57 -0800

    create and fill CustomerSpecification for Mapping Criteria Filter Search Operators

---
+++ src/main/java/com/example/finalprojectthirdphase/util/specification/CustomerSpecification.java
@@ -0,0 +1,37 @@
+package com.example.finalprojectthirdphase.util.specification;
+
+import com.example.finalprojectthirdphase.entity.Customer;
+import com.example.finalprojectthirdphase.entity.criteria.SearchCriteria;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import org.springframework.data.jpa.domain.Specification;
+
+@AllArgsConstructor
+public class CustomerSpecification implements Specification<Customer> {
+
+    private SearchCriteria criteria;
+
+    @Override
+    public Predicate toPredicate(@NonNull
+                                 Root<Customer> root, @NonNull CriteriaQuery<?> query, @NonNull CriteriaBuilder builder) {
+
+        return switch (criteria.getOperation()) {
+            case EQUALITY -> builder.equal(root.get(criteria.getKey()), criteria.getValue());
+            case NEGATION -> builder.notEqual(root.get(criteria.getKey()), criteria.getValue());
+            case GREATER_THAN -> builder.greaterThan(root.get(
+                    criteria.getKey()), criteria.getValue().toString());
+            case LESS_THAN -> builder.lessThan(root.get(
+                    criteria.getKey()), criteria.getValue().toString());
+            case LIKE -> builder.like(root.get(
+                    criteria.getKey()), criteria.getValue().toString());
+            case STARTS_WITH -> builder.like(root.get(criteria.getKey()), criteria.getValue() + "%");
+            case ENDS_WITH -> builder.like(root.get(criteria.getKey()), "%" + criteria.getValue());
+            case CONTAINS -> builder.like(root.get(
+                    criteria.getKey()), "%" + criteria.getValue() + "%");
+        };
+    }
+}
