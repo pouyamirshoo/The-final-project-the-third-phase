a2bf581484714033dfa8c24de547ebcb36c5ebed
pc <poyatajhiz59@gmail.com>
2024-07-04 16:24:41 -0800

    change SubDuty to subDutyReturn

--- src/main/java/com/example/finalprojectthirdphase/dto/RequestReturn.java
+++ src/main/java/com/example/finalprojectthirdphase/dto/RequestReturn.java
@@ -1,11 +1,10 @@
 package com.example.finalprojectthirdphase.dto;
 
 import com.example.finalprojectthirdphase.entity.Expert;
-import com.example.finalprojectthirdphase.entity.SubDuty;
 
 import java.util.List;
 
 public record RequestReturn(Integer id,
                             Expert expert,
-                            List<SubDuty> subDutyList) {
+                            List<SubDutyReturn> subDutyList) {
 }
