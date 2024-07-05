5b4b39b180b80f1d32209a9f14c58666feda2f53
pc <poyatajhiz59@gmail.com>
2024-07-04 16:32:10 -0800

    create and fill WrongImageInputException extended class

---
+++ src/main/java/com/example/finalprojectthirdphase/exception/WrongImageInputException.java
@@ -0,0 +1,7 @@
+package com.example.finalprojectthirdphase.exception;
+
+public class WrongImageInputException extends RuntimeException {
+    public WrongImageInputException(String message) {
+        super(message);
+    }
+}
