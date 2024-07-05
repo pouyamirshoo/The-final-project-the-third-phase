9cc354e03f8ec779a4772b064d007a2279c2450e
pc <poyatajhiz59@gmail.com>
2024-07-04 16:02:04 -0800

    create and fill SearchOperation Enum class for Filter Search

---
+++ src/main/java/com/example/finalprojectthirdphase/entity/criteria/SearchOperation.java
@@ -0,0 +1,18 @@
+package com.example.finalprojectthirdphase.entity.criteria;
+
+public enum SearchOperation {
+    EQUALITY, NEGATION, GREATER_THAN, LESS_THAN, LIKE, STARTS_WITH, ENDS_WITH, CONTAINS;
+
+    public static final String[] SIMPLE_OPERATION_SET = {":", "!", ">", "<", "~"};
+
+    public static SearchOperation getSimpleOperation(char input) {
+        return switch (input) {
+            case ':' -> EQUALITY;
+            case '!' -> NEGATION;
+            case '>' -> GREATER_THAN;
+            case '<' -> LESS_THAN;
+            case '~' -> LIKE;
+            default -> null;
+        };
+    }
+}
