045939aad7096ea64463df36027be195fb3c1c51
pc <poyatajhiz59@gmail.com>
2024-07-04 16:10:26 -0800

    create and fill SubDutyReturn Record DTO class for SubDuty Entity

---
+++ src/main/java/com/example/finalprojectthirdphase/dto/SubDutyReturn.java
@@ -0,0 +1,9 @@
+package com.example.finalprojectthirdphase.dto;
+
+public record SubDutyReturn(Integer id,
+                            String subDutyName,
+                            Integer price,
+                            String description,
+                            DutyReturn duty) {
+
+}
