504f97e9513d3bf1d344c67da6faba4a59dd9dbe
pc <poyatajhiz59@gmail.com>
2024-07-04 16:29:17 -0800

    create and fill InvalidInputInformationException extended class

---
+++ src/main/java/com/example/finalprojectthirdphase/exception/InvalidInputInformationException.java
@@ -0,0 +1,7 @@
+package com.example.finalprojectthirdphase.exception;
+
+public class InvalidInputInformationException extends RuntimeException {
+    public InvalidInputInformationException(String message) {
+        super(message);
+    }
+}
