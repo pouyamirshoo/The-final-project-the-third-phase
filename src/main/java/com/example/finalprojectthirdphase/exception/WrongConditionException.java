4dd255a8ca19530aef172af58836da2718a83b7b
pc <poyatajhiz59@gmail.com>
2024-07-04 16:31:27 -0800

    create and fill WrongConditionException extended class

---
+++ src/main/java/com/example/finalprojectthirdphase/exception/WrongConditionException.java
@@ -0,0 +1,7 @@
+package com.example.finalprojectthirdphase.exception;
+
+public class WrongConditionException extends RuntimeException {
+    public WrongConditionException(String message) {
+        super(message);
+    }
+}
