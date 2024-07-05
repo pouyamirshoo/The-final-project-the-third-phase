d112fa33489386cac83c38be7c71c7826571901e
pc <poyatajhiz59@gmail.com>
2024-07-04 16:30:44 -0800

    create and fill NotFoundException extended class

---
+++ src/main/java/com/example/finalprojectthirdphase/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.finalprojectthirdphase.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String massage) {
+        super(massage);
+    }
+}
