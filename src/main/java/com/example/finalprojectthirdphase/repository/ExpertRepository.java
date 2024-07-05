8363a94d76376fa93b008acde329773a7e95eec5
pc <poyatajhiz59@gmail.com>
2024-07-04 16:40:30 -0800

    create and fill ExpertRepository extended interface

---
+++ src/main/java/com/example/finalprojectthirdphase/repository/ExpertRepository.java
@@ -0,0 +1,26 @@
+package com.example.finalprojectthirdphase.repository;
+
+
+import com.example.finalprojectthirdphase.entity.Expert;
+import com.example.finalprojectthirdphase.entity.enums.ExpertCondition;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ExpertRepository extends JpaRepository<Expert, Integer>, JpaSpecificationExecutor<Expert> {
+    Optional<Expert> findByUsername(String username);
+
+    Optional<Expert> findByEmail(String email);
+
+    Optional<Expert> findByPhoneNumber(String phoneNumber);
+
+    Optional<Expert> findByPostalCode(String postalCode);
+
+    Optional<Expert> findByNationalCode(String nationalCode);
+
+    Optional<Expert> findByUsernameAndPassword(String username, String password);
+
+    List<Expert> findByExpertCondition(ExpertCondition expertCondition);
+}
