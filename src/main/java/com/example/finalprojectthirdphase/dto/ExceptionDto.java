6a5256b948cb2244699f021da19e7dc67e0db68b
pc <poyatajhiz59@gmail.com>
2024-07-04 16:17:19 -0800

    create and fill ExceptionDto Record DTO class for Handling Exceptions

---
+++ src/main/java/com/example/finalprojectthirdphase/dto/ExceptionDto.java
@@ -0,0 +1,7 @@
+package com.example.finalprojectthirdphase.dto;
+
+import java.time.LocalDateTime;
+
+public record ExceptionDto(String message,
+                           LocalDateTime localDateTime) {
+}
