9f397562eac3fd19982983d38dc701ff53cc4a90
pc <poyatajhiz59@gmail.com>
2024-07-11 16:51:18 -0800

    add Spring security needed fields to Admin entity class

--- src/main/java/com/example/finalprojectthirdphase/entity/Admin.java
+++ src/main/java/com/example/finalprojectthirdphase/entity/Admin.java
@@ -1,18 +1,56 @@
 package com.example.finalprojectthirdphase.entity;
 
+import com.example.finalprojectthirdphase.entity.enums.Role;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.SoftDelete;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @ToString
 @SoftDelete
 @SuperBuilder
 @Entity
-public class Admin extends Person{
+public class Admin extends Person implements UserDetails {
+
+    @Enumerated(EnumType.STRING)
+    Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
