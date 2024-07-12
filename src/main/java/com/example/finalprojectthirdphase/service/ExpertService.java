fce444554c92f3d90add6e3f3eb847b8e672f60d
pc <poyatajhiz59@gmail.com>
2024-07-11 17:23:03 -0800

    add needed methods for the forth phase in ExpertService

--- src/main/java/com/example/finalprojectthirdphase/service/ExpertService.java
+++ src/main/java/com/example/finalprojectthirdphase/service/ExpertService.java
@@ -1,7 +1,10 @@
 package com.example.finalprojectthirdphase.service;
 
 import com.example.finalprojectthirdphase.entity.Expert;
+import com.example.finalprojectthirdphase.entity.Offer;
 import com.example.finalprojectthirdphase.entity.enums.ExpertCondition;
+import com.example.finalprojectthirdphase.entity.enums.OfferCondition;
+import com.example.finalprojectthirdphase.entity.enums.Role;
 import com.example.finalprojectthirdphase.exception.DuplicateInformationException;
 import com.example.finalprojectthirdphase.exception.NotFoundException;
 import com.example.finalprojectthirdphase.exception.NotMatchPasswordException;
@@ -12,9 +15,10 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -23,8 +27,11 @@ import java.util.List;
 public class ExpertService {
 
     private final ExpertRepository expertRepository;
+    private final EmailService emailService;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final OfferService offerService;
 
-    public Expert saveExpert(Expert expert) {
+    public void saveExpert(Expert expert) {
         if (expertRepository.findByUsername(expert.getUsername()).isPresent()) {
             log.error("duplicate username can not insert");
             throw new DuplicateInformationException("duplicate username can not insert");
@@ -45,15 +52,42 @@ public class ExpertService {
             log.error("duplicate nationalCode can not insert");
             throw new DuplicateInformationException("duplicate nationalCode can not insert");
         }
-        return expertRepository.save(expert);
+        expert.setRole(Role.ROLE_EXPERT);
+        expert.setPassword(passwordEncoder.encode(expert.getPassword()));
+
+        String token = UUID.randomUUID().toString();
+        expert.setVerificationToken(token);
+        expertRepository.save(expert);
+
+        String confirmationUrl = "http://localhost:8080/verify-ExpertEmail?token=" + token;
+        emailService.sendEmail(expert.getEmail(), "Email Verification", "Click the link to verify your email: " + confirmationUrl);
     }
 
+    public Expert findByToken(String token) {
+        return expertRepository.findByVerificationToken(token).orElse(null);
+    }
+
+    public Expert findByUsername(String username) {
+        return expertRepository.findByUsername(username).orElseThrow(() ->
+                new NotFoundException("expert with id " + username + " not founded"));
+    }
+
+    public Optional<Expert> findByUsernameOP(String username) {
+        return expertRepository.findByUsername(username);
+    }
+
+    public List<Expert> findSubDutyExperts(String subDutyName) {
+        return expertRepository.findBySubDuties_SubDutyName(subDutyName);
+    }
 
     public Expert signInExpert(String username, String password) {
-        return expertRepository.findByUsernameAndPassword(username, password).orElseThrow(() ->
-                new NotFoundException("wrong username or password"));
+        Expert expert = findByUsername(username);
+        String enCodePassword = expert.getPassword();
+        if (!passwordEncoder.matches(password, enCodePassword)) {
+            throw new NotFoundException("wrong username or password");
+        }
+        return expert;
     }
-
 
     public Expert findById(int id) {
         return expertRepository.findById(id).orElseThrow(() ->
@@ -62,11 +96,12 @@ public class ExpertService {
 
     public Expert UpdatePassword(String oldPassword, String newPassword, String confirmPassword,
                                  Expert expert) {
-        if (!expert.getPassword().equals(oldPassword))
+        String enCodePassword = expert.getPassword();
+        if (!passwordEncoder.matches(oldPassword, enCodePassword))
             throw new NotMatchPasswordException("wrong password entered");
         if (!newPassword.equals(confirmPassword))
             throw new NotMatchPasswordException("different password entered");
-        expert.setPassword(newPassword);
+        expert.setPassword(passwordEncoder.encode(newPassword));
         return expertRepository.save(expert);
     }
 
@@ -92,7 +127,7 @@ public class ExpertService {
     }
 
     public void setRate(int rate, Expert expert) {
-        expert.setRate(rate);
+        expert.setRate(rate + expert.getRate());
         expertRepository.save(expert);
     }
 
@@ -100,6 +135,27 @@ public class ExpertService {
         int rate = expert.getRate();
         if (rate < 0)
             updateExpertCondition(ExpertCondition.BLOCKED, expert);
+    }
+
+    public Map<Integer, Integer> findAllByOfferCount() {
+        List<Expert> experts = expertRepository.findAll();
+        Map<Integer, Integer> offerCountById = new HashMap<>();
+        for (Expert expert : experts) {
+            offerCountById.put(expert.getId(), expert.getOffers().size());
+        }
+        return offerCountById;
+    }
+
+    public Map<Integer, Integer> findByDoneOfferCount() {
+        List<Offer> offers = offerService.findByOfferCondition(OfferCondition.DONE);
+        Map<Integer, Integer> offerDoneCountById = new HashMap<>();
+        for (Offer offer : offers) {
+            if (offerDoneCountById.containsKey(offer.getExpert().getId())) {
+                offerDoneCountById.replace(offer.getExpert().getId(), offerDoneCountById.get(offer.getExpert().getId()) + 1);
+            }
+            offerDoneCountById.put(offer.getExpert().getId(), 1);
+        }
+        return offerDoneCountById;
     }
 
     public void removeExpert(int id) {
