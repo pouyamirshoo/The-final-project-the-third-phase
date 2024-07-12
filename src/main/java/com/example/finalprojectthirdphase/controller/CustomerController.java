b96f4f91e3d0838854d0fcd48c0bf59016d7db07
pc <poyatajhiz59@gmail.com>
2024-07-11 17:24:39 -0800

    add needed methods for the forth phase in CustomerController

--- src/main/java/com/example/finalprojectthirdphase/controller/CustomerController.java
+++ src/main/java/com/example/finalprojectthirdphase/controller/CustomerController.java
@@ -4,6 +4,7 @@ import com.example.finalprojectthirdphase.dto.*;
 import com.example.finalprojectthirdphase.entity.*;
 import com.example.finalprojectthirdphase.entity.enums.OfferCondition;
 import com.example.finalprojectthirdphase.entity.enums.OrderCondition;
+import com.example.finalprojectthirdphase.exception.NotMatchPasswordException;
 import com.example.finalprojectthirdphase.mapper.CommentsMapper;
 import com.example.finalprojectthirdphase.mapper.CustomerMapper;
 import com.example.finalprojectthirdphase.mapper.OfferMapper;
@@ -15,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,35 +38,61 @@ public class CustomerController {
     private final CreatAndValidationDate creatAndValidationDate;
 
     @PostMapping("registerCustomer")
-    public ResponseEntity<CustomerReturn> registerCustomer(@Valid @RequestBody CustomerSaveRequest request) {
+    public ResponseEntity<?> registerCustomer(@Valid @RequestBody CustomerSaveRequest request) {
+        String password = request.password();
+        if (!password.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#!%&*])[A-Za-z0-9@#!%&*]{8}$")) {
+            throw new NotMatchPasswordException("password has to be 8 size and must contain at least 1 lower and upper case and 1 digit and 1 char");
+        }
         Customer mappedCustomer = CustomerMapper.INSTANCE.customerSaveRequestToModel(request);
-        Customer savedCustomer = customerService.saveCustomer(mappedCustomer);
-        return new ResponseEntity<>(CustomerMapper.INSTANCE.modelCustomerToSaveResponse(savedCustomer),
-                HttpStatus.CREATED);
+        customerService.saveCustomer(mappedCustomer);
+        return ResponseEntity.ok("Verify email by the link sent on your email address");
+    }
+
+    @RequestMapping(value = "/verify-email", method = {RequestMethod.GET, RequestMethod.POST})
+    public ResponseEntity<?> confirmUserAccount(@RequestParam("token") String confirmationToken) {
+
+        Customer customer = customerService.findByToken(confirmationToken);
+
+        if (customer == null) {
+            return ResponseEntity.badRequest().body("Error: Couldn't verify email");
+        }
+        if (customer.isEnabled()) {
+            return ResponseEntity.badRequest().body("Error: already verified email");
+        }
+        customer.setEnabled(true);
+        customerService.forceSave(customer);
+        return ResponseEntity.ok("Email verified successfully!");
     }
 
     @GetMapping("customer_SignIn")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public ResponseEntity<CustomerReturn> customerSignIn(@RequestParam String username, String password) {
         Customer customer = customerService.singInCustomer(username, password);
         return new ResponseEntity<>(CustomerMapper.INSTANCE.modelCustomerToSaveResponse(customer),
                 HttpStatus.FOUND);
     }
 
-    @GetMapping("find_By_CustomerId")
-    public ResponseEntity<CustomerReturn> findByCustomerId(@RequestParam int id) {
-        Customer customer = customerService.findById(id);
-        return new ResponseEntity<>(CustomerMapper.INSTANCE.modelCustomerToSaveResponse(customer),
-                HttpStatus.FOUND);
-    }
-
     @GetMapping("update_Customer_Password")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public String updateCustomerPassword(@RequestParam int id, String oldPassword, String newPassword, String confirmPassword) {
+        if (!newPassword.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#!%&*])[A-Za-z0-9@#!%&*]{8}$")) {
+            throw new NotMatchPasswordException("password has to be 8 size and must contain at least 1 lower and upper case and 1 digit and 1 char");
+        }
         Customer customer = customerService.findById(id);
         Customer updatedCustomer = customerService.UpdatePassword(oldPassword, newPassword, confirmPassword, customer);
-        return updatedCustomer.getPassword();
+        return "your password changed dear " + updatedCustomer.getFirstname();
+    }
+
+    @GetMapping("findOne_Order_Offers")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
+    public List<OfferReturn> findOneOrderOffers(@RequestParam int id) {
+        Order order = orderService.findById(id);
+        List<Offer> offers = offerService.findOrderOffers(order);
+        return OfferMapper.INSTANCE.listOfferToSaveResponse(offers);
     }
 
     @PostMapping("make_Order")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public ResponseEntity<OrderReturn> makeOrder(@Valid @RequestBody OrderSaveRequest orderSaveRequest) {
         Date creatDate = creatAndValidationDate.currentTime().toDate();
         Date needExpert = creatAndValidationDate.insertDate(orderSaveRequest.needExpertDate()).toDate();
@@ -78,6 +106,7 @@ public class CustomerController {
     }
 
     @GetMapping("allOrders_OfOneCustomer")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public List<OrderReturn> allOrdersOfOneCustomer(@RequestParam int id) {
         Customer customer = customerService.findById(id);
         List<Order> orders = orderService.findCustomerOrders(customer);
@@ -85,6 +114,7 @@ public class CustomerController {
     }
 
     @GetMapping("find_By_OrderId")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public ResponseEntity<OrderReturn> findByOrderId(@RequestParam int id) {
         Order order = orderService.findById(id);
         return new ResponseEntity<>(OrderMapper.INSTANCE.modelOrderToSaveResponse(order),
@@ -92,12 +122,14 @@ public class CustomerController {
     }
 
     @GetMapping("findBy_OrderCondition")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public List<OrderReturn> findByOrderCondition(@RequestParam OrderCondition orderCondition) {
         List<Order> orders = orderService.findByOrderCondition(orderCondition);
         return OrderMapper.INSTANCE.listOrderToSaveResponse(orders);
     }
 
     @GetMapping("findBy_SubDutyOrders")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public List<OrderReturn> findBySubDutyOrders(@RequestParam int id) {
         SubDuty subDuty = subDutyService.findById(id);
         List<Order> orders = orderService.findSubDutyOrders(subDuty);
@@ -105,6 +137,7 @@ public class CustomerController {
     }
 
     @GetMapping("show_offers_ByExpertRate")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public List<OfferReturn> showOffersByExpertRate(@RequestParam int id) {
         Order order = orderService.findById(id);
         List<Offer> offers = offerService.setOffersByExpertRate(order);
@@ -113,6 +146,7 @@ public class CustomerController {
     }
 
     @GetMapping("show_offers_ByPrice")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public List<OfferReturn> showOffersByPrice(@RequestParam int id) {
         Order order = orderService.findById(id);
         List<Offer> offers = offerService.setOffersByPrice(order);
@@ -120,6 +154,7 @@ public class CustomerController {
     }
 
     @GetMapping("accept_Offer")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public OrderReturn acceptOffer(@RequestParam int orderId, int offerId) {
         Order order = orderService.findById(orderId);
         Offer offer = offerService.findById(offerId);
@@ -130,6 +165,7 @@ public class CustomerController {
     }
 
     @GetMapping("make_Order_OnGoing")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public OrderReturn makeOrderOnGoing(@RequestParam int id) {
         Order order = orderService.findById(id);
         Order onGoingOrder = orderService.makeOrderOngoing(order);
@@ -137,6 +173,7 @@ public class CustomerController {
     }
 
     @GetMapping("make_Order_Done")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public OrderReturn makeOrderDone(@RequestParam int id) {
         Order order = orderService.findById(id);
         Order doneOrder = orderService.makeOrderDone(order);
@@ -144,6 +181,7 @@ public class CustomerController {
     }
 
     @PostMapping("save_Comments")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public ResponseEntity<CommentsReturn> saveComments(@Valid @RequestBody CommentsSaveRequest commentsSaveRequest) {
         Comments mappedComments = CommentsMapper.INSTANCE.INSTANCE.commentsSaveRequestToModel(commentsSaveRequest);
         Comments savedComments = commentsService.saveComment(mappedComments);
@@ -152,7 +190,15 @@ public class CustomerController {
                 HttpStatus.CREATED);
     }
 
+    @GetMapping("show_balance")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
+    public String showCustomerBalance(@RequestParam int id) {
+        Customer customer = customerService.findById(id);
+        return Integer.toString(customer.getCustomerBalance());
+    }
+
     @GetMapping("add_Balance")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public ResponseEntity<CustomerReturn> addBalance(@RequestParam int id, int price) {
         Customer customer = customerService.findById(id);
         Customer updatedCustomer = customerService.addBalance(customer, price);
@@ -161,6 +207,7 @@ public class CustomerController {
     }
 
     @GetMapping("payBy_Balance")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public ResponseEntity<CustomerReturn> payByBalance(@RequestParam int id) {
         Order order = orderService.findById(id);
         Customer customer = customerService.payOrderPriceFromBalance(order);
@@ -169,12 +216,14 @@ public class CustomerController {
     }
 
     @DeleteMapping("delete_customer")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public String removeCustomer(@RequestParam int id) {
         customerService.removeCustomer(id);
         return "customer and All relations have been deleted";
     }
 
     @DeleteMapping("delete_order")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public String removeOrder(@RequestParam int id) {
         orderService.removeOrder(id);
         return "order and All relations have been deleted";
