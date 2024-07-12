e556e70293b8b813b6d3e2f2ec10a16030d33ef6
pc <poyatajhiz59@gmail.com>
2024-07-11 17:22:17 -0800

    add needed methods for the forth phase in AdminService

--- src/main/java/com/example/finalprojectthirdphase/service/AdminService.java
+++ src/main/java/com/example/finalprojectthirdphase/service/AdminService.java
@@ -1,16 +1,16 @@
 package com.example.finalprojectthirdphase.service;
 
-import com.example.finalprojectthirdphase.entity.Admin;
-import com.example.finalprojectthirdphase.entity.Expert;
-import com.example.finalprojectthirdphase.entity.Request;
-import com.example.finalprojectthirdphase.entity.SubDuty;
+import com.example.finalprojectthirdphase.entity.*;
+import com.example.finalprojectthirdphase.entity.enums.Role;
 import com.example.finalprojectthirdphase.exception.NotFoundException;
 import com.example.finalprojectthirdphase.repository.AdminRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -21,14 +21,30 @@ public class AdminService {
     private final ExpertService expertService;
     private final RequestService requestService;
     private final SubDutyService subDutyService;
+    private final BCryptPasswordEncoder passwordEncoder;
 
-    public void saveAdmin(Admin admin) {
-        adminRepository.save(admin);
+    public Admin saveAdmin(Admin admin) {
+        admin.setRole(Role.ROLE_ADMIN);
+        admin.setPassword(passwordEncoder.encode(admin.getPassword()));
+        return adminRepository.save(admin);
     }
 
-    public void adminSignIn(String username, String password) {
-        adminRepository.findByUsernameAndPassword(username, password).orElseThrow(() ->
-                new NotFoundException("wrong username or password"));
+    public Admin adminSignIn(String username, String password) {
+        Admin admin = findByUsername(username);
+        String enCodePassword = admin.getPassword();
+        if (!passwordEncoder.matches(password, enCodePassword)) {
+            throw new NotFoundException("wrong username or password");
+        }
+        return admin;
+    }
+
+    public Admin findByUsername(String username) {
+        return adminRepository.findByUsername(username).orElseThrow(() ->
+                new NotFoundException("customer with id " + username + " not founded"));
+    }
+
+    public Optional<Admin> findByUsernameOP(String username) {
+        return adminRepository.findByUsername(username);
     }
 
     public Expert addExpertToSubDutyAuto(Expert expert) {
