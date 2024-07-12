92edceb2f2d960f0e94b070c7409bc527f9992e2
pc <poyatajhiz59@gmail.com>
2024-07-11 17:07:38 -0800

    add Spring security needed fields to Customer entity class

--- src/main/java/com/example/finalprojectthirdphase/entity/Customer.java
+++ src/main/java/com/example/finalprojectthirdphase/entity/Customer.java
@@ -1,5 +1,6 @@
 package com.example.finalprojectthirdphase.entity;
 
+import com.example.finalprojectthirdphase.entity.enums.Role;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
@@ -7,7 +8,11 @@ import lombok.*;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.SoftDelete;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @Getter
@@ -18,15 +23,46 @@ import java.util.List;
 @SuperBuilder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
-@JsonIdentityInfo(scope = Customer.class,generator= ObjectIdGenerators.PropertyGenerator.class, property="id")
-public class Customer extends Person {
+@JsonIdentityInfo(scope = Customer.class, generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class Customer extends Person implements UserDetails {
+
     @Column(name = "customer_balance")
     Integer customerBalance;
+
+    @Enumerated(EnumType.STRING)
+    Role role;
+
+    boolean isEnabled;
+
+    String verificationToken;
+
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     List<Order> orders;
+
     @PrePersist
     public void defaultValues() {
         if (customerBalance == null)
             customerBalance = 0;
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
 }
